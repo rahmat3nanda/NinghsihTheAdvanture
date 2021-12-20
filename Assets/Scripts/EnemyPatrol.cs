a84302a016b87fd43898c711bba34d070a7a566d
Firman Asdiansyah <72846881+firmanasdiansyah@users.noreply.github.com>
2021-12-21 06:47:52 +0700

    enemy2&backsound

---
+++ Assets/Scripts/EnemyPatrol.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyPatrol : MonoBehaviour
+{
+
+    private Rigidbody2D fa;
+
+    public float KecepatanGerak;
+
+    public bool memutar;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        memutar = true;
+        fa = this.GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (memutar)
+        {
+            fa.velocity = new Vector2(KecepatanGerak, fa.velocity.y);
+            transform.rotation = Quaternion.Euler(0f, 180f, 0f);
+        }
+        else
+        {
+            fa.velocity = new Vector2(-KecepatanGerak, fa.velocity.y);
+            transform.rotation = Quaternion.Euler(0f, 0f, 0f);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Puter"))
+        {
+            memutar = !memutar;
+        }
+    }
+}
